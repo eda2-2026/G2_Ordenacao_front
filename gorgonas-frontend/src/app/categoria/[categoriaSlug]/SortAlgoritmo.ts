a629f816dd5e9548f794a7a70cc481701428239e
andrehsb <andrehsb06@gmail.com>
2026-05-11 00:51:48 -0300

    feat: Implementa função de counting auxiliar

--- gorgonas-frontend/src/app/categoria/[categoriaSlug]/SortAlgoritmo.ts
+++ gorgonas-frontend/src/app/categoria/[categoriaSlug]/SortAlgoritmo.ts
@@ -1,6 +1,33 @@
 import { ProdutoParaCard } from "@/components/ProductRow";
 
-function countingSortPreco(arr: ProdutoParaCard[], exp: number): void {}
+// Função auxiliar (Counting Sort)
+function countingSortPreco(arr: ProdutoParaCard[], exp: number): void {
+  const n = arr.length;
+  const output = new Array(n);
+  const count = new Array(10).fill(0);
+
+  for (let i = 0; i < n; i++) {
+    const precoCentavos = Math.floor(Number(arr[i].preco) * 100);
+    const digito = Math.floor(precoCentavos / exp) % 10;
+    count[digito]++;
+  }
+
+  for (let i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+  }
+
+  for (let i = n - 1; i >= 0; i--) {
+    const precoCentavos = Math.floor(Number(arr[i].preco) * 100);
+    const digito = Math.floor(precoCentavos / exp) % 10;
+    output[count[digito] - 1] = arr[i];
+    count[digito]--;
+  }
+
+  for (let i = 0; i < n; i++) {
+    arr[i] = output[i];
+  }
+}
+
 // Função principal (Radix Sort)
 export function radixSortPreco(arr: ProdutoParaCard[], ordem: 'asc' | 'desc' = 'asc'): ProdutoParaCard[] {
   if (arr.length === 0) return arr;
